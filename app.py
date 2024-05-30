0459b316f00ccfe089aa8edacc75aa524a42bcee
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-30 13:26:35 +0530

    app.py

--- app.py
+++ app.py
@@ -19,31 +19,32 @@ st.set_page_config(page_title="Bid Response Evaluation AI ", layout="wide")
 
 video_html = """
 		<style>
-
 		#myVideo {
 		  position: fixed;
 		  right: 0;
 		  bottom: 0;
 		  min-width: 100%; 
 		  min-height: 100%;
+		  filter: brightness(20%); /* Adjust the brightness to make the video darker */
 		}
-
+		
 		.content {
 		  position: fixed;
 		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
+		  background: rgba(0, 0, 0, 0.5); /* Adjust the transparency as needed */
 		  color: #f1f1f1;
 		  width: 100%;
 		  padding: 20px;
 		}
-
 		</style>	
 		<video autoplay muted loop id="myVideo">
-		  <source src="https://assets.mixkit.co/videos/1610/1610-720.mp4")>
+		  <source src="https://assets.mixkit.co/videos/1610/1610-720.mp4" type="video/mp4">
 		  Your browser does not support HTML5 video.
 		</video>
-        """
+		"""
+
 st.markdown(video_html, unsafe_allow_html=True)
+
 
 st.markdown("""
 <style>
