e1b29903b67601277dd72e96ecfc9504d194e300
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-05-31 11:47:29 +0530

    app.py

--- app.py
+++ app.py
@@ -238,8 +238,8 @@ def main():
                 prompt=prompt,
                 verbose=True
             )	
-	    response = chat_llm_chain.predict(raw_text=raw_text)
-	    st.write(response)
+            response = chat_llm_chain.predict(raw_text=raw_text)
+            st.write(response)
 
 
 if __name__ == "__main__":
