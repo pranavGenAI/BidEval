5e68149c48564c2f0b1ae2c7794a58579f490c1e
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-11 20:54:36 +0530

    app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def login():
     col4, col5, col6, col7, col8, col9 = st.columns([1, 1, 1, 1, 1, 1])
     st.markdown("")
     st.markdown("")
-    with col7:
+    with col4:
         st.markdown("")
         st.markdown("")
         st.markdown("")
